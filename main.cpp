1c8cb89430958d492c5e46096405da838944d7cb
xxc2269 <1289647471@qq.com>
2025-06-18 13:05:46 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+#include<graphics.h>
+
+int WINAPI WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nShowCmd)
+{
+    initgraph(1280, 720);
+    MessageBox(NULL, TEXT("一个简单的Win32应用程序"), TEXT("消息窗口"), MB_OK);
+    return 0;
+
+}
